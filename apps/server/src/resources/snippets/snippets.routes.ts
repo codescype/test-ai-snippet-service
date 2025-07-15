677c6b8b62d386662d4914e4fe5c91bc9648a3a0
Codescype <engineer.codescype@gmail.com>
2025-07-15 07:56:20 +0100

    feat(server): add validation to create snippet route

--- apps/server/src/resources/snippets/snippets.routes.ts
+++ apps/server/src/resources/snippets/snippets.routes.ts
@@ -1,5 +1,9 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 
 export const snippetsRoutes = new Elysia().group('/snippets', (app) => {
-  return app.post('/', 'Welcome to Create Snippet Route');
+  return app.post('/', 'Welcome to Create Snippet Route', {
+    body: t.Object({
+      text: t.String(),
+    }),
+  });
 });
