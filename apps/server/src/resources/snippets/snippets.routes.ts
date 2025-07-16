944ec4713acb59f72bd8859127742631a2444d49
Codescype <engineer.codescype@gmail.com>
2025-07-16 18:08:08 +0100

    feat(server): add an instance of Snippets Controller to the snippets route

--- apps/server/src/resources/snippets/snippets.routes.ts
+++ apps/server/src/resources/snippets/snippets.routes.ts
@@ -1,4 +1,9 @@
 import { Elysia, t } from 'elysia';
+import { SnippetsController } from './snippets.controller';
+
+export const snippetsRoutes = new Elysia()
+  // Add an instance of Snippets Controller to the snippets route
+  .decorate('snippetsController', new SnippetsController())
 
 export const snippetsRoutes = new Elysia().group('/snippets', (app) => {
   return app.post('/', 'Welcome to Create Snippet Route', {
