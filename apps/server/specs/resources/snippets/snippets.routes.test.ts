f769bd8bbf0b4e5352f25d18d58b7665fbe48ed5
Codescype <engineer.codescype@gmail.com>
2025-08-13 16:07:20 +0100

    test: move tests to its separate specs folder

---
+++ apps/server/specs/resources/snippets/snippets.routes.test.ts
@@ -0,0 +1,80 @@
+import { describe, it, expect, vi } from 'vitest';
+import { treaty } from '@elysiajs/eden';
+
+import { app } from '../../../src/app';
+import { aiService } from '../../../src/services/ai.service';
+import { prisma } from '../../../src/prisma/prisma';
+
+// Set up Elysia treaty for tests
+const request = treaty(app);
+
+// Set up mocks
+vi.mock('../../services/ai.service');
+vi.mock('../../prisma/prisma', () => ({
+  prisma: {
+    snippet: {
+      create: vi.fn(),
+      findMany: vi.fn(),
+      findUnique: vi.fn(),
+    },
+  },
+}));
+
+describe('Snippets Routes', () => {
+  // Create a mock snippet
+  const mockText =
+    'Test text with over fifty words to ensure it meets the requirement for length, covering various topics like technology and innovation in a meaningful way for testing purposes.';
+  const mockSummary = 'Mock summary';
+  const mockSnippet = {
+    id: 'mock-id',
+    text: mockText,
+    summary: mockSummary,
+  };
+
+  describe('POST /snippets - Create a Snippet', () => {
+    it('returns a 422 validation error status when text is not provided', async () => {
+      // @ts-expect-error so we can test the error.
+      const { status } = await request.snippets.post();
+
+      expect(status).toBe(422);
+    });
+
+    it('creates a snippet with summarized text', async () => {
+      vi.mocked(aiService.generateSummary).mockResolvedValue(mockSummary);
+      vi.mocked(prisma.snippet.create).mockResolvedValue(mockSnippet);
+
+      const response = await request.snippets.post({ text: mockText });
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual(mockSnippet);
+    });
+  });
+
+  describe('GET /snippets - Get all Snippets', () => {
+    it('returns all snippets', async () => {
+      vi.mocked(prisma.snippet.findMany).mockResolvedValue([mockSnippet]);
+
+      const response = await request.snippets.get();
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual([mockSnippet]);
+    });
+  });
+
+  describe('GET /snippets/:id - Find a Snippet', () => {
+    it('returns a 500 not error status when wrong id is provided', async () => {
+      vi.mocked(prisma.snippet.findUnique).mockRejectedValue(new Error());
+      const { status } = await request.snippets({ id: 'incorrect-id' }).get();
+
+      expect(status).toBe(500);
+    });
+
+    it('returns the found snippet', async () => {
+      vi.mocked(prisma.snippet.findUnique).mockResolvedValue(mockSnippet);
+      const response = await request.snippets({ id: mockSnippet.id }).get();
+
+      expect(response.status).toBe(200);
+      expect(response.data).toEqual(mockSnippet);
+    });
+  });
+});
