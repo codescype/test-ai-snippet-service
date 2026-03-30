04703263a8b22136db3b246f0e310cdd59378e86
Codescype <engineer.codescype@gmail.com>
2026-03-30 02:28:50 +0100

    chore(prisma): move prisma generated files to the project top-level generated folder

--- apps/server/src/prisma/prisma-client.ts
+++ apps/server/src/prisma/prisma-client.ts
@@ -1,4 +1,4 @@
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
-const { PrismaClient } = require('../../generated/prisma/client/index.js');
+const { PrismaClient } = require('../generated/prisma/client/index.js');
 export { PrismaClient };
